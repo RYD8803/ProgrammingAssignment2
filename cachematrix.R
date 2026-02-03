2247ec8a59d6a78fd45440fca4c5f34786b72274
Rafaelangelo Yudhistira Dharmawangsa <rafaelangeloyudhistira@gmail.com>
2026-02-03 21:50:52 +0700

    Editing the CacheMatrix for Coursera Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,30 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL #x is the matrix, m is the inverse one (for now it has a value of NULL)
+    }
+    get <- function() x
+    setinv <- function(solve) m <<- solve #Store the inverse after calculation
+    getinv <- function() m
+    list(set = set, get = get,#Lexical scoping
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getinv()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m) #Checking cache,; It won't make another calculation if the inverse already exist
+    }
+    data <- x$get()
+    m <- solve(data, ...) #compute and cache
+    x$setinv(m) 
+    m        ## Return a matrix that is the inverse of 'x'
 }
